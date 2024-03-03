d0cc5e6b47a73ab6539fe0ca74bb94e875d31997
Minakshi <146201694+minak23shi@users.noreply.github.com>
2024-03-03 08:50:44 +0000

    Project1

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import*
+from tkinter import messagebox
+from tkinter import ttk
+root=Tk()
+root.title("Login page")
+root.geometry("1500x888")
+frame=Frame(relief=GROOVE,bd=5,bg="grey")
+frame.place(x=0,y=0,width=665,height=50)
+L=Label(frame,text="Login Page",font=("standard",15,"underline"),fg="red").place(x=250,y=0)
+frame2=Frame(relief=GROOVE,bd=5,bg="pink")
+frame2.place(x=0,y=)
